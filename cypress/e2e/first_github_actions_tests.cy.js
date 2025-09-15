36a87d459f14375b3ef33acd125ed1d59d4f168e
Petr Fifka <petrfifk@gmail.com>
2025-09-15 19:11:48 +0200

    add login header test

--- cypress/e2e/first_github_actions_tests.cy.js
+++ cypress/e2e/first_github_actions_tests.cy.js
@@ -1,7 +1,14 @@
 describe("Cypress GitHub Actions Demo", () => {
-  it("Open PMtool and fill username", () => {
+  beforeEach(() => {
     cy.visit("https://tredgate.com/pmtool");
+  });
+
+  it("Open PMtool and fill username", () => {
     cy.get("#username").type("admin");
     cy.get("#username").should("have.value", "admin");
+  });
+
+  it("Login Header Check", () => {
+    cy.get("h3.form-title").should("be.visible");
   });
 });
